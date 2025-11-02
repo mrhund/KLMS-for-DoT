32dd1ff2e4033b6fc1478bbfc1d4e800f212b7c1
MrHund <schlauer_hund@hotmail.com>
2025-11-02 20:46:33 +0100

    QR Scanner #10

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -10,8 +10,8 @@ use App\Messenger\MailingHookNotification;
 use App\Service\GroupService;
 use App\Service\TicketService;
 use Doctrine\ORM\EntityManagerInterface;
-use Endroid\QrCode\Builder\Builder;
-use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\SvgWriter;
 use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
@@ -23,7 +23,6 @@ use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\DelayStamp;
 use Symfony\Component\Mime;
 use Twig\Environment;
-use function htmlspecialchars;
 
 class EmailService
 {
@@ -286,20 +285,13 @@ class EmailService
     private function generateTicketQrCodeHtml(string $code): string
     {
         try {
-            $result = Builder::create()
-                ->writer(new PngWriter())
-                ->data($code)
-                ->size(200)
-                ->margin(8)
-                ->build();
+            $qrCode = QrCode::create($code)
+                ->setSize(200)
+                ->setMargin(8);
 
-            $escapedCode = htmlspecialchars($code, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+            $result = (new SvgWriter())->write($qrCode);
 
-            return sprintf(
-                '<img src="%s" alt="Ticket QR-Code" title="Ticket-Code %s" style="max-width:200px;height:auto;" />',
-                $result->getDataUri(),
-                $escapedCode
-            );
+            return sprintf('<div class="ticket-qr">%s</div>', $result->getString());
         } catch (\Throwable) {
             return '';
         }
