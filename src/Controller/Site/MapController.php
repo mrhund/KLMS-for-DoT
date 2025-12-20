25b80a4f0116766699c3f092e4e86475e6eb632b
Bernhard Rastorfer <bernhard@rastorfer.net>
2025-12-20 13:47:10 +0000

    Implemented Map Feature

---
+++ src/Controller/Site/MapController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Site;
+
+use App\Service\GeocodingService;
+use App\Service\MapService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: '/map', name: 'map')]
+class MapController extends AbstractController
+{
+    public function __construct(
+        private readonly MapService $mapService,
+        private readonly GeocodingService $geocodingService,
+    ) {
+    }
+
+    #[Route(path: '', name: '')]
+    public function index(): Response
+    {
+        return $this->render('site/map/index.html.twig', [
+            'map' => $this->mapService->buildUserMap(),
+            'markerCount' => $this->mapService->getUserMarkerCount(),
+            'centerAddress' => $this->mapService->getCenterAddress(),
+            'apiKeyMissing' => !$this->geocodingService->hasApiKey(),
+        ]);
+    }
+}
