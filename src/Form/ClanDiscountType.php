7669cd52ed89ffb779b797a9676cf3ee7bad653c
Bernhard Rastorfer <bernhard@rastorfer.net>
2025-12-28 16:17:23 +0000

    Resolve merge conflicts

--- src/Form/ClanDiscountType.php
+++ src/Form/ClanDiscountType.php
@@ -34,11 +34,7 @@ class ClanDiscountType extends AbstractType
         ]);
     }
 
-<<<<<<< HEAD
     public function configureOptions(OptionsResolver $resolver): void
-=======
-    public function configureOptions(OptionsResolver $resolver)
->>>>>>> lanbuddys
     {
         $resolver->setDefaults([
             'data_class' => ClanDiscount::class,
