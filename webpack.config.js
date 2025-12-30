c5182bda32e198e4d42714ec87997855b833a20c
Bernhard Rastorfer <bernhard@rastorfer.net>
2025-12-30 12:02:52 +0000

    SCSS correction

--- webpack.config.js
+++ webpack.config.js
@@ -42,9 +42,6 @@ Encore
     .addEntry('admin_checkin', './assets/js/admin/checkin.js')
     .addStyleEntry('email', './assets/css/email.scss')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/js/controllers.json')
-
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
